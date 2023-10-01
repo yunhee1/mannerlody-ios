775320157ce58f3bdaae9c9029fe463b8649d6b0
yunhee1 <alkrg007@naver.com>
2023-10-02 00:36:18 +0900

    Feat: Add tab bar new icons

--- src/navigations/Tab.js
+++ src/navigations/Tab.js
@@ -1,16 +1,65 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home, Search, MySong, Settings } from "../screens";
+import { Image } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const TabNav = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="MySong" component={MySong} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen
+        name="홈"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              style={{ resizeMode: "contain", width: 25, height: 25 }}
+              source={require("../../assets/images/tab_home.png")}
+            />
+          ),
+          tabBarActiveTintColor: "#6635FA",
+        }}
+      />
+      <Tab.Screen
+        name="검색"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              style={{ resizeMode: "contain", width: 25, height: 25 }}
+              source={require("../../assets/images/tab_search.png")}
+            />
+          ),
+          tabBarActiveTintColor: "#6635FA",
+        }}
+      />
+      <Tab.Screen
+        name="내 음악"
+        component={MySong}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              style={{ resizeMode: "contain", width: 25, height: 25 }}
+              source={require("../../assets/images/tab_like.png")}
+            />
+          ),
+          tabBarActiveTintColor: "#6635FA",
+        }}
+      />
+      <Tab.Screen
+        name="설정"
+        component={Settings}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              style={{ resizeMode: "contain", width: 25, height: 25 }}
+              source={require("../../assets/images/tab_settings.png")}
+            />
+          ),
+          tabBarActiveTintColor: "#6635FA",
+        }}
+      />
     </Tab.Navigator>
   );
 };
